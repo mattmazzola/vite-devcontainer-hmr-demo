ee933309d952952d801e2344ae368da473abf6b3
Matthew Mazzola <mattmazzola@live.com>
2024-03-12 02:29:42 +0000

    Add .devcontainer files

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,7 @@
+git config --global safe.directory '*'
+git config --global core.editor "code --wait"
+git config --global pager.branch false
+
+npm install
+
+echo "postCreateCommand.sh completed!"
